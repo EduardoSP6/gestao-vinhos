7150c8d621d69019344585bcf249b5e572d22842
EduardoSP6 <eduardo.sp6@gmail.com>
2017-08-15 21:15:48 -0300

    First commit

---
+++ src/main/java/com/algaworks/vinhos/GestaoVinhosApplication.java
@@ -0,0 +1,21 @@
+package com.algaworks.vinhos;
+
+import java.util.Locale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.i18n.FixedLocaleResolver;
+
+@SpringBootApplication
+public class GestaoVinhosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoVinhosApplication.class, args);
+	}
+	
+	@Bean
+	public FixedLocaleResolver localeResolver() {
+		return new FixedLocaleResolver(new Locale("pt", "BR"));
+	}
+}
